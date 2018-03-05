1eed33b2a7a0042662fd0a6faacf1e5ecfd222c6
Valerie Marchese <valeriemarchese@Valeries-MacBook-Air.local>
2018-03-04 20:05:25 -0700

    added some js

---
+++ assets/javascript/app.js
@@ -0,0 +1,37 @@
+
+var questions = 0;
+var correctPicks =0;
+var incorrectPicks =0;
+
+
+
+var trivaQuestions = [
+        q1 = {
+            question: "Who was the first porn star to be CONTRACTED by an adult studio?",
+            correct: 2,
+            multChoice: ["Linda Lovelace", "Marilyn Chambers", "Ginger Lynn", "Traci Lords"]
+            
+
+        },
+        q2 = {
+            question: "What famous porn star left teaching junior high Spanish for a long and prosperous career in adult entertainment?",
+            correct: 1,
+            multChoice:["Janna Jameson", "Juli Ashton", "Lisa Ann", "Tera Patrick"]
+        },
+        q3 = {
+            question: "What blond hottie appeared alongside Jason Mewes in the Hollywood hit 'Zack And Miri Make A Porno'?",
+            correct: 3,
+            multChoice: ["Angie Savage", "Alexis Texas", "Anita Blond", "Katie Morgan"]
+        },
+        q4 = {
+            question: "Which U.S. men's magazine was the first to show pubic hair?",
+            correct: 2,
+            multChoice: [" Playboy", "Hustler", "Penthouse","Stag"]
+        },
+
+]
+
+
+
+
+
