07d1f3056f95fa967d969546b65dc69373613582
Valerie Marchese <valeriemarchese@Valeries-MacBook-Air.local>
2018-03-05 22:05:18 -0700

    js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,36 +1,62 @@
 
-var questions = 0;
-var correctPicks =0;
-var incorrectPicks =0;
+const questions = [
+    {
+        question: "Who was the first porn star to be CONTRACTED by an adult studio?",
+        choices: ["Linda Lovelace", "Marilyn Chambers", "Ginger Lynn", "Traci Lords"],
+        validAnswer: 2
+    }, 
+    {
+        question: "What famous porn star left teaching junior high Spanish for a long and prosperous career in adult entertainment?",
+        multChoice:["Janna Jameson", "Juli Ashton", "Lisa Ann", "Tera Patrick"],
+        validAnswer: 1,
 
-
-
-var trivaQuestions = [
-        q1 = {
-            question: "Who was the first porn star to be CONTRACTED by an adult studio?",
-            correct: 2,
-            multChoice: ["Linda Lovelace", "Marilyn Chambers", "Ginger Lynn", "Traci Lords"]
-            
-
-        },
-        q2 = {
-            question: "What famous porn star left teaching junior high Spanish for a long and prosperous career in adult entertainment?",
-            correct: 1,
-            multChoice:["Janna Jameson", "Juli Ashton", "Lisa Ann", "Tera Patrick"]
-        },
-        q3 = {
-            question: "What blond hottie appeared alongside Jason Mewes in the Hollywood hit 'Zack And Miri Make A Porno'?",
-            correct: 3,
-            multChoice: ["Angie Savage", "Alexis Texas", "Anita Blond", "Katie Morgan"]
-        },
-        q4 = {
-            question: "Which U.S. men's magazine was the first to show pubic hair?",
-            correct: 2,
-            multChoice: [" Playboy", "Hustler", "Penthouse","Stag"]
-        },
-
-]
-
+    },
+    {
+        question: "What blond hottie appeared alongside Jason Mewes in the Hollywood hit 'Zack And Miri Make A Porno'?",
+        multChoice: ["Angie Savage", "Alexis Texas", "Anita Blond", "Katie Morgan"],
+        validAnswer: 3
+},
+{
+        question: "Which U.S. men's magazine was the first to show pubic hair?",
+        multChoice: ["Playboy", "Hustler", "Penthouse","Stag"],
+        validAnswer:2
+    },
+];
+$(document).ready(function(){
+    
+    $("#startUpGame").on('click',function(){
+        setupGame();
+    });
+});
+function setupGame(){
+    $.each(questions,function(i){
+        var html = '';
+                        html = setQuestion(this.question,i);
+                        html = html.concat(setChoices(this.choices,i));
+        $('#theGame').append(html);
+                });
+}
+function setQuestion(question,questionIndex){
+                var setId = "#question"+questionIndex;
+    var html = '<h3 id="'+setId+'">'+question+'</h3>';
+    return html;
+}
+function setChoices(choices,questionIndex){
+    var html = '';
+    $.each(choices,function(choiceIndex){
+        html = html.concat('<h3 onClick="checkAnswer('+questionIndex+','+choiceIndex+');">'+this.toString()+'</h3>');
+    });
+    return html;
+}
+function checkAnswer(question,choice){
+    if(choice === questions[question].validAnswer){
+        //Correct Answer
+console.log("Correct Answer");
+    } else {
+        //Incorrect Answer
+console.log("Incorrect Answer");
+    }
+}
 
 
 
